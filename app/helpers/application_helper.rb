6f2b7b6ffd03591db98f69d9b407b2ec0ea44141
radigin <radigin@192.168.1.2>
2015-02-06 00:41:36 +0400

    Оформление основной части.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,20 +18,29 @@ module ApplicationHelper
       if item.has_key?(:children)
         item[:children].each do |x| 
           if x[:controller].to_s == controller_name and 
-              x[:action].to_s == action_name
+              x[:action].to_s == menu_action(action_name, controller_name)
             x[:class] = 'side-bar-active'
             item[:class] = 'side-bar-open'
           end
         end
       else  
         if item[:controller].to_s == controller_name and 
-            item[:action].to_s == action_name
+            item[:action].to_s == menu_action(action_name, controller_name)
           item[:class] = 'side-bar-active'
         end
       end
     end
   end
 
+  def menu_action(real_action, real_controller)
+    case real_action
+    when 'edit', 'new', 'create', 'update', 'destroy', 'show'
+      'index'
+    else
+      real_action
+    end
+  end
+  
   def generate_top_bar_items()
     result = []
     if @current_role_user.is_admin?
@@ -45,5 +54,13 @@ module ApplicationHelper
       ]} 
     end
     result   
+  end
+
+  def error_messages_for(object)
+    render 'error_messages_for', object: object
+  end
+
+  def error_messages_for_attr(object, attr)
+    render 'error_messages_for_attr', object: object, attr: attr
   end
 end
