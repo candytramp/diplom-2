62d2e13078d6d1ba4e26e17c91c08c31ff4b0039
radigin <radigin@192.168.1.2>
2015-02-08 22:54:29 +0400

    Оформление первой страницы.

--- app/helpers/nobody_helper.rb
+++ app/helpers/nobody_helper.rb
@@ -1,2 +1,14 @@
 module NobodyHelper
+  def start_links()
+    result = []
+    if @current_role_user.is_admin?
+      result << {:name => 'Пользователи',
+         :controller => :users, :action => :index,
+         :icon => 'fa-users'}
+      result << {:name => 'Роли',
+         :controller => :roles, :action => :index,
+         :icon => 'fa-cubes'}
+    end
+    result
+  end
 end
