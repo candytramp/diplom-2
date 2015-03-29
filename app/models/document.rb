099a5c0a9525e7a0977ffc96b88f7422b0557ae4
soa40 <soa40@mail.msiu.ru>
2015-03-29 15:46:22 +0300

    Media-object

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,7 @@ class Document < ActiveRecord::Base
 	serialize :creator_data
 	has_paper_trail
   belongs_to :owner, polymorphic: true
-	has_attached_file :paper, styles: {thumbnail: ["60x60#", :jpg]}
+	has_attached_file :paper, styles: {thumb: ["100x100#", :jpg]}
 	validates_attachment :paper, content_type: { content_type: ['application/pdf', 'image/jpeg', 'image/tiff', 'image/bmp', 'image/png' ]}
+  validates :paper, attachment_presence: true
 end
